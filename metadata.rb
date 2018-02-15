f073c4babae60e9117dc3a821b300f03e6cd91b0
Gavin Staniforth <gavin@usemarkup.com>
2018-02-15 21:36:53 +0000

    feat: improve the support

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'global-composer'
 license 'MIT Licence'
 description 'an omnibus of packages to ease installing php'
-version '0.1.1'
+version '1.0.0'
 
 maintainer 'Gavin Staniforth'
 maintainer_email 'gavin@usemarkup.com'
@@ -9,7 +9,6 @@ maintainer_email 'gavin@usemarkup.com'
 source_url 'https://github.com/usemarkup/chef-composer'
 
 supports 'centos', '>= 6.8'
+supports 'centos', '>= 7.4'
 
-depends 'php-omnibus'
-
-chef_version '>= 12.7' if respond_to?(:chef_version)
+chef_version '>= 12.6' if respond_to?(:chef_version)
